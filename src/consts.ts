a07f248e780f5cc541c5e2ae556e937e5d308375
Waris Reshi <warisareshi@gmail.com>
2026-02-04 12:24:41 +0530

    v1 astro based blog

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Waris Reshi';
+export const SITE_DESCRIPTION = "waris reshi's personal site - entrepreneur, developer, and athlete based in srinagar. co-founder of asent and focused on technology, applied ai, and human performance.";
